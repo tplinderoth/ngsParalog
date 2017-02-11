378c1eace4e2786eed5583328b3e6bbd83aca38b
tplinderoth <tylerp.linderoth@gmail.com>
2017-02-11 01:09:25 -0800

    add exception handling

--- ngsParalog.h
+++ ngsParalog.h
@@ -7,7 +7,7 @@
 #include <iostream>
 #include "Argparser.h"
 
-const char* version = "1.3.0"; // 7 Nov 2016
+const char* version = "1.3.1"; // 10 Feb 2017
 
 // FUNCTION PROTOTYPES
 
@@ -16,14 +16,15 @@ int parseInput (int argc, char** argv, std::string& infile, std::string& outfile
 		bool& printML, int& numericGrad, int& verbose, const char* v = version);
 void boundConditions (double min [], double max [], int nbounds [], int dim);
 int setOptim (Optim& model, bool isalt, int verb);
-void printLR (const std::string chr, const unsigned int pos, Optim* null, Optim* alt, double lr, std::ostream& os, bool printml);
-unsigned int nameLength (std::string name);
+void printLR (const std::string chr, const unsigned int pos, Optim* null, Optim* alt, double lr, std::ostream& os,
+		const unsigned int nameBuffer, bool printml);
+unsigned int calcIdBuffer (const unsigned int nameLength);
 unsigned int numCovered (const Pileup* data, const unsigned int mincov);
 int doLR (Argparser* params); // calculate per site LR of mismapped reads
 int doRegion(); // do HMM to find coordinates of mismapping
 int processPileup (std::istream& indat, std::ostream& os, Optim* altmodel, Optim* nullmodel, double (*fn)(const double x[], const void*),
 		void (*dfn)(const double x[], double y[], const void*), const double qoffset, const double minq, const unsigned int mindepth,
-		const unsigned int minind, int printML, int verbose);
+		const unsigned int minind, int printML);
 void endMessage (int status);
 
 #endif /* NGSPARALOG_H_ */
