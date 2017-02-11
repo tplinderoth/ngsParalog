d1759db22bffdeece0153f8366d1f9f0eb06790c
tplinderoth <tylerp.linderoth@gmail.com>
2017-02-11 01:08:18 -0800

    add exception classes

--- generalUtils.h
+++ generalUtils.h
@@ -77,6 +77,24 @@ private:
         size_t sz;
 };
 
+// EXCEPTION CLASSES
+
+class AssertStyleException : public std::exception
+{
+public:
+        AssertStyleException (const char* err = NULL);
+        virtual const char* what () const throw();
+protected:
+        const char* _error;
+};
+
+class PreConditionException : public AssertStyleException
+{
+public:
+	PreConditionException (const char* err = NULL);
+	virtual const char* what () const throw();
+};
+
 // EXCEPTION MESSAGE FORMATTER CLASS
 
 class ExceptionFormatter
@@ -91,14 +109,25 @@ public:
 		return *this;
 	}
 
-	std::string str() const {return stream_.str();}
-	operator std::string () const {return stream_.str();} // so that anything requiring std::string can take type ExceptionFormatter
+	// for converting to std::string
 
-	enum ConvertToString {to_str};
-	std::string operator>> (ConvertToString) {return stream_.str();}
+	std::string str() const {return stream_.str();}
+	operator std::string () const {return stream_.str();} // cast operator so that anything requiring std::string can take type ExceptionFormatter
+
+	enum ConvertToString {to_str}; // ConvertToString is a variable within the ExceptionFormatter class that can only take 'to_str' as its value
+	std::string operator>> (ConvertToString) {return stream_.str();} // overloaded >> so that ExceptionFormater::to_str will return stream_ as a string
+
+	// for converting to C style strings
+    const char* cstr() const {return stream_.str().c_str();}
+    operator const char* () const {return stream_.str().c_str();}
+
+    enum ConvertToCString {to_cstr};
+    const char* operator>> (ConvertToCString) {return stream_.str().c_str();}
 
 private:
 	std::stringstream stream_;
+	//ExceptionFormatter(const ExceptionFormatter&);
+	//ExceptionFormatter& operator= (ExceptionFormatter&);
 };
 
 // FUNCTION PROTOTYPES
